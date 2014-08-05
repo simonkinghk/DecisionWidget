d96cafa10e6e392a3ff5aa1f5b55405af4473a52
simonkinghk <kingsinghk@gmail.com>
2014-08-05 17:00:41 +0800

    add widget part, not compile yet

---
+++ src/com/simonkinghk/decisionwidget/AppWidgetMain.java
@@ -0,0 +1,35 @@
+package com.simonkinghk.decisionwidget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+public class AppWidgetMain extends AppWidgetProvider {
+
+	/** Called when the activity is first created. */
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		final int IDs = appWidgetIds.length;
+
+		for (int i = 0; i < IDs; i++) {
+			int appWidgetId = appWidgetIds[i];
+			updateAppWidget(context, appWidgetManager, appWidgetId);
+		}
+	}
+
+	public void onDeleted(Context context, int[] appWidgetIds) {
+
+	}
+
+	private void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+		// TODO Auto-generated method stub
+		CharSequence text;
+		text = "my test";
+
+		RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_main);
+		views.setTextViewText(R.id.HelloTextView01, text);
+
+		appWidgetManager.updateAppWidget(appWidgetId, views);
+
+	}
+}
