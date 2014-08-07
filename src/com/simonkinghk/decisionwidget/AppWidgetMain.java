b540232aa3a905f09998c87df91ce46cf4a7faeb
simonkinghk <kingsinghk@gmail.com>
2014-08-07 14:16:09 +0800

    add widget

--- src/com/simonkinghk/decisionwidget/AppWidgetMain.java
+++ src/com/simonkinghk/decisionwidget/AppWidgetMain.java
@@ -1,16 +1,22 @@
 package com.simonkinghk.decisionwidget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 public class AppWidgetMain extends AppWidgetProvider {
+	
+	public static String WIDGET_ACTION = "WidgetOnClickAction";
 
 	/** Called when the activity is first created. */
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 		final int IDs = appWidgetIds.length;
-
+		
 		for (int i = 0; i < IDs; i++) {
 			int appWidgetId = appWidgetIds[i];
 			updateAppWidget(context, appWidgetManager, appWidgetId);
@@ -22,12 +28,29 @@ public class AppWidgetMain extends AppWidgetProvider {
 	}
 
 	private void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
-		CharSequence text;
-		text = SharePreferenceInstance.getInstance().generateRandomNumber(SharePreferenceInstance.getInstance().getPreference()) + "";
+		// add widget button action
+		Intent intent = new Intent(context, AppWidgetMain.class);
+		intent.setAction(WIDGET_ACTION);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
 
 		RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_main);
-		views.setTextViewText(R.id.HelloTextView01, text);
+		views.setOnClickPendingIntent(R.id.showButton, pendingIntent);
 
+		//views.setTextViewText(R.id.HelloTextView01, text);
 		appWidgetManager.updateAppWidget(appWidgetId, views);
+	}
+	
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		super.onReceive(context, intent);
+		if (intent.getAction().equals(WIDGET_ACTION)) {
+			//do widget action here
+			AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+			RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_main);
+			ComponentName watchWidget = new ComponentName(context, AppWidgetMain.class);
+
+			remoteViews.setTextViewText(R.id.showButton, SharePreferenceInstance.getInstance().generateRandomNumber(SharePreferenceInstance.getInstance().getPreference()) + "");
+			appWidgetManager.updateAppWidget(watchWidget, remoteViews);
+		}
 	}
 }
