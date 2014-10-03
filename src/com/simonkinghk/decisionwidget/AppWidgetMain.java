c063eacb1b66d02f2b41d849ada2d74a58b1bbd9
simonkinghk <kingsinghk@gmail.com>
2014-10-03 11:51:23 +0800

    fix bug & add toast

--- src/com/simonkinghk/decisionwidget/AppWidgetMain.java
+++ src/com/simonkinghk/decisionwidget/AppWidgetMain.java
@@ -8,6 +8,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 public class AppWidgetMain extends AppWidgetProvider {
 	
@@ -15,29 +16,15 @@ public class AppWidgetMain extends AppWidgetProvider {
 
 	/** Called when the activity is first created. */
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-		final int IDs = appWidgetIds.length;
+		RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_main);
 		
-		for (int i = 0; i < IDs; i++) {
-			int appWidgetId = appWidgetIds[i];
-			updateAppWidget(context, appWidgetManager, appWidgetId);
-		}
-	}
-
-	public void onDeleted(Context context, int[] appWidgetIds) {
-
-	}
-
-	private void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
 		// add widget button action
 		Intent intent = new Intent(context, AppWidgetMain.class);
 		intent.setAction(WIDGET_ACTION);
 		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
-
-		RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_main);
 		views.setOnClickPendingIntent(R.id.showButton, pendingIntent);
 
-		//views.setTextViewText(R.id.HelloTextView01, text);
-		appWidgetManager.updateAppWidget(appWidgetId, views);
+		appWidgetManager.updateAppWidget(appWidgetIds, views);
 	}
 	
 	@Override
@@ -57,6 +44,8 @@ public class AppWidgetMain extends AppWidgetProvider {
 				}}, 1000);
 			remoteViews.setTextViewText(R.id.showButton, MyApplication.getAppContext().getResources().getString(R.string.generating_widget));
 			appWidgetManager.updateAppWidget(watchWidget, remoteViews);
+			
+			Toast.makeText(MyApplication.getAppContext(), "Range: 1-"+SharePreferenceInstance.getInstance().getPreference(), Toast.LENGTH_SHORT).show();
 		}
 	}
 }
